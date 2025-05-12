cc7514cb0e7e32fd9023c5c034ef4e6bb682af0e
cooler-SAI <coolerme86@gmail.com>
2025-05-12 20:39:54 +0300

    feat: Implement HTTP handler and remove "Hello World"
    This commit implements an HTTP handler that responds with a greeting message including the pod's hostname. It also removes the initial "Hello World" print statement from the main function, focusing the program on its HTTP server functionality.

--- main.go
+++ main.go
@@ -1,6 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received request from %s for path %s", r.RemoteAddr, r.URL.Path)
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal("Can't get Hostname: ", err)
+	}
+	_, err2 := fmt.Fprintf(w, "Hello from Kubernetes! I'm serving from pod: %s\n", hostname)
+	if err2 != nil {
+		log.Fatal("Can't write response: ", err2)
+		return
+	}
+}
 
 func main() {
 
